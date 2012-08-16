9655178ee1d709c35114f5c133f0220eba0648fd
Edgar Perez <edgarp313@gmail.com>
2012-08-15 22:56:44 -0600

    Fresh app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimeListing::Application.config.secret_token = 'faba8373fd546ad9231a3f4239202c9eb793d930269edb9a6761faea817026f95da3f825acabe02053eb7a17d2fb24665310f3f645273b95499c61520c89cca2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AnimeListing::Application.config.session_store :cookie_store, :key => '_anime_listing_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AnimeListing::Application.config.session_store :active_record_store
